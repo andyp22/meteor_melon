eadbbd0c914c13f205d80ffce20c1a6960a0b28e
Andrew Page <andrew.page32@gmail.com>
2016-04-14 10:46:27 -0400

    Add basic Meteor project structure and packages

---
+++ client/lib/environment.js
@@ -0,0 +1,13 @@
+/*
+ * Session Defaults
+ */
+//Session.setDefault( 'example_var', -1 );
+/*
+ * Router settings for the iron:router package.
+ */
+Router.configure({
+	layoutTemplate: 'ApplicationLayout',
+	data: {
+		title: "Meteor Melon"
+	}
+});
